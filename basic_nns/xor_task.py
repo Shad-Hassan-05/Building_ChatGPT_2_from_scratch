d8b61243e15d3c0981a193f7b8c71b66a5f45b29
Shad Hassan <shad.h.hassan.27@dartmouth.edu>
2025-10-22 11:36:05 -0400

    Add files via upload

---
+++ basic_nns/xor_task.py
@@ -0,0 +1,37 @@
+from create_net import create_net
+from train import train
+from load_dataset import load_dataset
+from torch import random, save
+
+random.manual_seed(0)
+# %%% DO NOT EDIT ABOVE
+
+# Specify the lead_data arguments
+data_path = "xor_dataset.pt"
+mean_subtraction = False
+normalization = False
+
+# retrieve data set specificed above
+xor_dataset = load_dataset(data_path, mean_subtraction, normalization)
+
+# specify the network architecture
+in_features = 2
+out_size = 2
+hidden_units = [5] # number of neurons
+non_linearity = ['tanH']
+
+# create a network base on the architecture
+net = create_net(in_features, hidden_units, non_linearity, out_size)
+
+# specify the training opts
+train_opts = {'num_epochs': 100,
+              'lr' : 0.1,
+              'momentum' : 0.9,
+              'batch_size' : 4,
+              'weight_decay' : 0,
+              'step_size' : 25,
+              'gamma' : 1 }
+
+# train  and save the model
+train(net, xor_dataset, train_opts)
+save(net, 'xor_solution.pt')
